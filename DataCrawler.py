aaccafde4c6c69449acb459e65b9a5dffc11513c
chickenshifu_local <tobias@Lingnas-MacBook-Pro.local>
2021-01-03 11:19:11 +0100

    DataCrawler saves to .csv files with julian date

--- DataCrawler.py
+++ DataCrawler.py
@@ -3,10 +3,12 @@
 
 #DataCrawler Class is passed a list of tuples which it then will parse the heck out of it from yahoo.finance.
 
-
-
-import urllib.request
+import json
+import csv
+import requests
 import os 
+import time
+import random
 
 class DataCrawler:
 
@@ -30,8 +32,10 @@ class DataCrawler:
             targets = list()
 
             for symbol in self.symbols:
+                
+                baseUrl = 'https://query2.finance.yahoo.com/v8/finance/chart/{}?symbol={}&period1=0&period2=9999999999&interval=1d'.format(symbol, symbol)
 
-                baseUrl = 'https://de.finance.yahoo.com/quote/{}/history?period1=01&period2=1609545600&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true'.format(symbol)
+                # baseUrl = 'https://de.finance.yahoo.com/quote/{}/history?period1=01&period2=1609545600&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true'.format(symbol)
                 targets.append((symbol, baseUrl))
         
             return targets
@@ -39,7 +43,6 @@ class DataCrawler:
 
         else:
             None
-
 
 
 
@@ -54,23 +57,44 @@ class DataCrawler:
             return filename
 
 
+        def exportToCsv(results):
+
+            with open(filename, 'w') as out:
+                csvOut = csv.writer(out)
+                csvOut.writerow(['t', 'o', 'h', 'l', 'c'])
+                for row in results:
+                    csvOut.writerow(row)
+            print('Generated {}'.format(filename))
+
+
         for url in self.targets:
+            
             filename = makeFilename(url[0])
+            results = list()
 
             try:
 
-                urllib.request.urlretrieve(url[1], filename)
+                targetsJsonResponse = requests.get(url[1]).text
+                timestampList = json.loads(targetsJsonResponse)['chart']['result'][0]['timestamp']
+                openList = json.loads(targetsJsonResponse)['chart']['result'][0]['indicators']['quote'][0]['open']
+                highList = json.loads(targetsJsonResponse)['chart']['result'][0]['indicators']['quote'][0]['high']
+                lowList = json.loads(targetsJsonResponse)['chart']['result'][0]['indicators']['quote'][0]['low']
+                closeList = json.loads(targetsJsonResponse)['chart']['result'][0]['indicators']['quote'][0]['close']
 
-            except urllib.request.ContentTooShortError as e:
-                outfile = open(filename, "w")
-                outfile.write(e.content)
-                outfile.close()
+                time.sleep(3 + random.randint(0,2))
+                
+                for i in timestampList:
+                    results.append((i, openList.pop(), highList.pop(), lowList.pop(), closeList.pop()))
 
 
+                exportToCsv(results)
 
-            #Stopped here, change code so that in files only date, open, high, low, close, volume is stored
+                
+            except requests.exceptions.RequestException as e:  # This is the correct syntax
+                raise SystemExit(e)
 
-            
+
+        
 
 
 
@@ -79,5 +103,6 @@ if __name__ == '__main__':
     symbols = list()
     symbols = ('ADJ.DE', 'BMW.DE', 'DTE.DE', 'SIE.DE', 'PLUG')
     
+
     dc = DataCrawler(symbols)
     dc.crawlWebsite()
